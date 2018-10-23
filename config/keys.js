6ce692ad7848da91748963aaf5c43f13a1e02d98
“Elizabeth <lizzyadeline@gmail.com>
2018-10-22 19:02:48 -0500

    Initial commit and simple backend complete

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://elizabeth:password1@ds139523.mlab.com:39523/mern-shopping-list'
+};
---
+++ routes/api/items.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router();
+
+// Item model
+const Item = require('../../models/Item');
+
+// @route GET api/items (get all items)
+router.get('/', (req, res) => {
+    Item.find()
+        .sort({date: -1})
+        .then(items => res.json(items))
+});
+
+// @route POST api/items (create an item)
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name : req.body.name
+    });
+
+    newItem.save().then(item => res.json(item));
+});
+
+// @route DELETE api/items/:id (delete an item)
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id).then(item => 
+        item.remove().then(() => res.json({success: true})))
+        .catch(err => res.status(404).json({success: false}));
+});
+
+
+module.exports = router;
